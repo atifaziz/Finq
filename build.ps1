b41979c2158ac9bdfbf70503d0a6a36770cd0c06
Atif Aziz <code@raboof.com>
2024-06-10 22:24:20 +0200

    Add command to build release package

--- build.ps1
+++ build.ps1
@@ -15,7 +15,17 @@ param(
     [Parameter(ParameterSetName = 'Pack')]
     [switch]$CI,
     [Parameter(ParameterSetName = 'Pack')]
-    [switch]$NoValidation
+    [switch]$NoValidation,
+    [Parameter(ParameterSetName = 'Release', Mandatory = $true, Position = 0)]
+    [switch]$Release,
+    [Parameter(ParameterSetName = 'Release', Mandatory = $true, Position = 1)]
+    [string]$CommitId,
+    [Parameter(ParameterSetName = 'Release')]
+    [string]$Label,
+    [Parameter(ParameterSetName = 'Release')]
+    [string]$Tag,
+    [Parameter(ParameterSetName = 'Release')]
+    [string]$DestinationPath = '.'
 )
 
 $ErrorActionPreference = 'Stop'
@@ -83,12 +93,7 @@ function Pack
         $versionSuffix = "ci-$($commitTime.ToUniversalTime().ToString('yyyyMMdd''t''HHmm'))"
     }
 
-    [string[]]$argv = @()
-    if ($versionSuffix)
-    {
-        $argv += '--version-suffix'
-        $argv += $VersionSuffix
-    }
+    [string[]]$argv = if ($versionSuffix) { @('--version-suffix', $versionSuffix) }
 
     dotnet pack --no-build @argv
 
@@ -121,11 +126,106 @@ function Pack
     }
 }
 
-if (-not $noToolRestore)
+function Release
 {
-    dotnet tool restore
+    [version]$ghVersion = `
+        gh --version |
+        # Example version string: gh version 2.50.0 (2024-05-29)
+        ? { $_ -match '(?<=gh +version +)[0-9]+(\.[0-9]+){2}\b' } |
+        % { $Matches[0] }
     if (!$?) {
-        throw "Restoring tools failed (exit code = $LASTEXITCODE)."
+        throw "GitHub CLI not found or is incompatible."
+    }
+
+    if ($ghVersion.Major -ne 2) {
+        throw "Unsupported GitHub CLI version: $ghVersion"
+    }
+
+    $run = `
+        gh run list -c $commitId -s completed -w build --json 'databaseId,number,displayTitle,conclusion' |
+        Select-Object -First 1 |
+        ConvertFrom-Json
+    if (!$?) {
+        throw "Listing GitHub workflow runs failed (exit code = $LASTEXITCODE)."
+    }
+
+    if ($run.conclusion -ne 'success') {
+        throw "Latest run (#$($run.number)) for commit $commitId did not succeed."
+    }
+
+    $runId = $run.databaseId
+    Write-Verbose "Downloading artifacts from run #$($runId)"
+
+    $runPath = Join-Path .temp gh runs $runId
+    gh run download -D $runPath -n nuget $runId
+    if (!$?) {
+        throw "Downloading artifacts (for $($runId)) failed (exit code = $LASTEXITCODE)."
+    }
+
+    [array]$runNupkgs = Get-ChildItem (Join-Path $runPath *.nupkg)
+
+    if ($runNupkgs.Count -ne 1) {
+        throw "Expected a single nupkg file but found $($runNupkgs.Count)."
+    }
+
+    $nupkgPath = $runNupkgs[0]
+
+    [string[]]$argv = if ($label) { @('--label', $label) }
+
+    $argv += $nupkgPath
+    dotnet nupkgwrench release @argv
+    if (!$?) {
+        throw "Releasing package failed (exit code = $LASTEXITCODE)."
+    }
+
+    [string]$nupkgPath = `
+        Get-ChildItem (Join-Path $runPath *.nupkg) |
+        Select-Object -ExpandProperty FullName
+
+    [xml]$nuspec = dotnet nupkgwrench nuspec show $nupkgPath
+    if (!$?) {
+        throw "Querying package metadata failed (exit code = $LASTEXITCODE)."
+    }
+
+    $repoUrl = [uri]$nuspec.package.metadata.repository.url
+
+    if (!$package.metadata.releaseNotes)
+    {
+        if (!$tag)
+        {
+            $pkgVersion = dotnet nupkgwrench version $nupkgPath
+            if (!$?) {
+                throw "Querying package version failed (exit code = $LASTEXITCODE)."
+            }
+
+            $tag = "v$pkgVersion"
+            Write-Warning "No tag specified. Assuming: $tag"
+        }
+
+        if ($repoUrl.Host -eq 'github.com')
+        {
+            dotnet nupkgwrench nuspec edit `
+                --property ReleaseNotes --value "https://github.com/atifaziz/Finq/releases/$tag" `
+                $nupkgPath
+            if (!$?) {
+                throw "Editing package failed (exit code = $LASTEXITCODE)."
+            }
+        }
+        else
+        {
+            Write-Warning "Release notes not set. Unsupported repository URL: $repoUrl"
+        }
+    }
+    elseif ($tag)
+    {
+        Write-Warning "Release notes already set! Ignoring tag: $tag"
+    }
+
+    Move-Item $nupkgPath $destinationPath
+
+    dotnet nupkgwrench nuspec show (Join-Path $destinationPath (Split-Path $nupkgPath -Leaf))
+    if (!$?) {
+        throw "Showing package failed (exit code = $LASTEXITCODE)."
     }
 }
 
